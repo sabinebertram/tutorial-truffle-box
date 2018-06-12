f7d60b142e13e1de37ffd9630e9c87bab796e541
sabinebertram <sabine.bertram@mailbox.org>
2018-06-12 08:25:41 +0200

    update

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
      networks: {
-          ganache: {
+          development: {
                host: "localhost",
                port: 7545,
                network_id: "*" // Match any network id
