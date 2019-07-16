25c59a485f90be4a459a4568e309cace4340c08b
Sabine <sabine.bertram@mailbox.org>
2019-07-16 10:13:45 +0200

    deploy only main contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,5 @@
-var Ownable = artifacts.require("./Ownable.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
-var ZombieFactory = artifacts.require("./ZombieFactory.sol");
-var ZombieFeeding = artifacts.require("./ZombieFeeding.sol");
-var ZombieHelper = artifacts.require("./ZombieHelper.sol");
-var ZombieAttack = artifacts.require("./ZombieAttack.sol");
 var ZombieOwnership = artifacts.require("./ZombieOwnership.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(Ownable);
-    deployer.deploy(SafeMath);
-    deployer.deploy(ZombieFactory);
-    deployer.deploy(ZombieFeeding);
-    deployer.deploy(ZombieHelper);
-    deployer.deploy(ZombieAttack);
     deployer.deploy(ZombieOwnership);
 };
