5e076d3a7fc0df94aa0809b048c3e10ea2e52b9d
Sabine <sabine.bertram@mailbox.org>
2020-01-15 17:34:40 +0100

    update contracts

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -6,37 +6,33 @@ import "./SafeMath.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    mapping (uint => address) zombieApprovals;
+  mapping (uint => address) zombieApprovals;
 
-    function balanceOf(address _owner) public view returns (uint256 _balance) {
-        return ownerZombieCount[_owner];
-    }
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
 
-    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
-        return zombieToOwner[_tokenId];
-    }
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
 
-    function _transfer(address _from, address _to, uint256 _tokenId) private {
-        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
-        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
-        zombieToOwner[_tokenId] = _to;
-        emit Transfer(_from, _to, _tokenId);
-    }
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
 
-    function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
-        _transfer(msg.sender, _to, _tokenId);
-    }
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    _transfer(_from, _to, _tokenId);
+  }
 
-    function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
-        zombieApprovals[_tokenId] = _to;
-        emit Approval(msg.sender, _to, _tokenId);
-    }
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _approved;
+    emit Approval(msg.sender, _approved, _tokenId);
+  }
 
-    function takeOwnership(uint256 _tokenId) public {
-        require(zombieApprovals[_tokenId] == msg.sender);
-        address owner = ownerOf(_tokenId);
-        _transfer(owner, msg.sender, _tokenId);
-    }
 }
